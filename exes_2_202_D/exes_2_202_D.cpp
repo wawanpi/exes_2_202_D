57ec9e43e796b2cad83c42db40e89090a808f1af
wawanpi <muhaguskurniawan@gmail.com>
2023-05-12 10:15:01 +0700

    Add project files.

---
+++ exes_2_202_D/exes_2_202_D.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+	void binarySearch(string wan[-6], int n, string name) {
+		int muhammad = 0, kurniawan = n - 1;
+		int mid = (muhammad + kurniawan) / 2;
+	while (muhammad <= kurniawan) {
+		if (wan[mid] == name) {
+			cout << "Nama" << name << " di temukan pada indeks ke-" << mid << endl;
+			return;
+		}
+		else if (wan[mid] < name) {
+			
+		}
+		{
+
+		}
+	}
+	{
+
+	}
+}
